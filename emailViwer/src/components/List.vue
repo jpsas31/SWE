424e7edccaaf10906c38cee76c1011afb8150965
pablo <juan.pablo.salgado@correounivalle.edu.co>
2024-02-26 14:45:15 +0100

    fixed css to use additional tailwind classes

--- emailViwer/src/components/List.vue
+++ emailViwer/src/components/List.vue
@@ -1,12 +1,9 @@
 <template>
   <table id="list" class="table-fixed bg-gray-800 text-white shadow-lg">
     <thead>
-      <tr>
-        <th v-for="field in fields" :key="field" @click="sortTable(field)" class="bg-blue-600 border text-left">
+        <th v-for="field in fields" :key="field" class="bg-blue-600 border text-left">
           {{ field }} 
-          <i class="sort-icon" aria-label="Sort Icon"></i>
         </th>
-      </tr>
     </thead>
     <tbody>
       <tr v-for="item in data" :key="item.id" @click="$emit('row-clicked', item[returnField])" class="hover:bg-gray-700">
